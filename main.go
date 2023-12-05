fc2e45cbfb62c396014ef67cfaa93ac201bd9653
Erik Bryant <erikbryantology@gmail.com>
2023-12-05 13:29:10 -0500

    Works!

--- main.go
+++ main.go
@@ -1,9 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"hash/crc32"
+	"image"
+	"log"
 )
+
+var (
+	filename = flag.String("filename", "", "Filename to bin")
+	x        = flag.Int("x", 270, "Upper left corner")
+	y        = flag.Int("y", 174, "Upper left corner")
+	width    = flag.Int("width", 1000, "Width")
+	height   = flag.Int("height", 60, "Height")
+)
+
+func usage() {
+	fmt.Println("Usage:")
+	fmt.Println()
+	fmt.Println("  Bin an image by a computing a checksum of [a portion of] the image")
+	fmt.Println("    binning -filename 'file.png' -x 0 -y 10 -width 100 -height 100")
+}
+
+func coordsToBounds(minX, minY int, width, height int) image.Rectangle {
+	return image.Rectangle{
+		Min: image.Point{Y: minY, X: minX},
+		Max: image.Point{Y: minY + height - 1, X: minX + width - 1},
+	}
+}
 
 func main() {
 	fmt.Println("Welcome to binning!")
+	flag.Parse()
+
+	if *filename == "" {
+		usage()
+		log.Fatal("You must supply a filename")
+	}
+
+	bounds := coordsToBounds(*x, *y, *width, *height)
+
+	slice, err := slicePNG(*filename, bounds)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	crc := crc32.ChecksumIEEE(slice)
+
+	fmt.Println(crc, *filename)
 }
---
+++ png.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"image"
+	"image/draw"
+	"image/png"
+	"os"
+)
+
+// readPNG reads a PNG file and returns it as a grayscale image
+func readPNG(filename string) (*image.Gray, error) {
+	// Open the PNG file
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	// Decode the RGB PNG file into an image
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert the RGB image to grayscale
+	grayscale := image.NewGray(img.Bounds())
+	draw.Draw(grayscale, grayscale.Bounds(), img, image.Point{}, draw.Src)
+
+	return grayscale, nil
+}
+
+func writePNG(filename string, src *image.Gray, bounds image.Rectangle) error {
+	// Create a blank image to receive the pixels
+	img := image.NewGray(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+
+	// Copy pixels from the source region
+	for y := 0; y < bounds.Dy(); y++ {
+		for x := 0; x < bounds.Dx(); x++ {
+			srcX := x + bounds.Min.X
+			srcY := y + bounds.Min.Y
+			img.Set(x, y, src.At(srcX, srcY))
+		}
+	}
+
+	// Write new image to a file
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if err := png.Encode(f, img); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// imageToSlice returns the selected pixels of the image in a slice
+func imageToSlice(img *image.Gray, bounds image.Rectangle) []uint8 {
+	s := []uint8{}
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			c := img.GrayAt(y, x)
+			s = append(s, c.Y)
+		}
+	}
+
+	return s
+}
+
+func slicePNG(filename string, bounds image.Rectangle) ([]byte, error) {
+	img, err := readPNG(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	err = writePNG("out_"+filename, img, bounds)
+	if err != nil {
+		return nil, err
+	}
+
+	return imageToSlice(img, bounds), nil
+}
